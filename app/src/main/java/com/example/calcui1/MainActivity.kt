3144ba929d2a5f738318c55441cd89d7c0beaa99
Gaurav Srivastava <52217208+gs1719@users.noreply.github.com>
2023-01-29 12:06:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/calcui1/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.calcui1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnPlus.setOnClickListener {
+        tvResult.text= (intValues(etInput1)+intValues(etInput2)).toString()
+
+        }
+            btnMinus.setOnClickListener {
+        tvResult.text= (intValues(etInput1)-intValues(etInput2)).toString()
+
+        }
+        btnDivision.setOnClickListener {
+        tvResult.text= (intValues(etInput1)/intValues(etInput2)).toString()
+
+        }
+        btnMultiply.setOnClickListener {
+        tvResult.text= (intValues(etInput1)*intValues(etInput2)).toString()
+
+        }
+    }
+
+    private fun intValues(x:EditText): Int {
+     return x.text.toString().toInt();
+    }
+}
