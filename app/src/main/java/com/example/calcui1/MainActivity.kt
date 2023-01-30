8658433fb6389a215a83ce3398726f5567629d1d
Gaurav Srivastava <52217208+gs1719@users.noreply.github.com>
2023-01-30 21:22:11 +0530

    Added button animation fixed app crashes by invalid input added btnPressed function

--- app/src/main/java/com/example/calcui1/MainActivity.kt
+++ app/src/main/java/com/example/calcui1/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.calcui1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -10,13 +12,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnPlus.setOnClickListener {
-        tvResult.text= (intValues(etInput1)+intValues(etInput2)).toString()
 
+        btnPressed()
+    }
+
+    private fun intValues(x: EditText): Int {
+        return try {
+            x.text.toString().toInt()
+        } catch (e: Exception) {
+            Toast.makeText(this, "Invalid Input", Toast.LENGTH_SHORT).show()
+            0
         }
-            btnMinus.setOnClickListener {
-        tvResult.text= (intValues(etInput1)-intValues(etInput2)).toString()
+    }
 
+
+    private fun btnAnimate(y: Button) {
+        //Set button alpha to zero
+        y.alpha = 0f
+
+        //Animate the alpha value to 1f and set duration as 1.5 secs.
+        y.animate().alpha(1f).duration = 500
+    }
+
+    private fun btnPressed() {
+        btnPlus.setOnClickListener {
+            tvResult.text = (intValues(etInput1) + intValues(etInput2)).toString()
+            btnAnimate(btnPlus)
+        }
+        btnMinus.setOnClickListener {
+            tvResult.text = (intValues(etInput1) - intValues(etInput2)).toString()
+            btnAnimate(btnMinus)
         }
         btnDivision.setOnClickListener {
             btnAnimate(btnDivision)
@@ -27,12 +52,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
         btnMultiply.setOnClickListener {
-        tvResult.text= (intValues(etInput1)*intValues(etInput2)).toString()
-
+            tvResult.text = (intValues(etInput1) * intValues(etInput2)).toString()
+            btnAnimate(btnMultiply)
         }
-    }
-
-    private fun intValues(x:EditText): Int {
-     return x.text.toString().toInt();
     }
 }
